5078c5b3348d6d2b720ae36047ca8e6e268e3d34
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-11-12 13:43:39 -0400

    AppDataBase

---
+++ app/src/main/java/edu/ucne/appliedbarbershop/data/local/AppDataBase.kt
@@ -0,0 +1,29 @@
+package edu.ucne.appliedbarbershop.data.local
+
+import androidx.room.Database
+import edu.ucne.appliedbarbershop.data.local.dao.BarberoDao
+import edu.ucne.appliedbarbershop.data.local.dao.CitaDao
+import edu.ucne.appliedbarbershop.data.local.dao.PerfilDao
+import edu.ucne.appliedbarbershop.data.local.dao.ServicioDao
+import edu.ucne.appliedbarbershop.data.local.models.Barbero
+import edu.ucne.appliedbarbershop.data.local.models.Perfil
+import edu.ucne.appliedbarbershop.data.local.models.Servicio
+import edu.ucne.appliedbarbershop.data.local.models.Cita
+
+@Database(
+    entities = [
+        Barbero::class,
+        Cita::class,
+        Perfil::class,
+        Servicio::class,
+    ],
+    version = 1,
+    exportSchema = false
+)
+
+abstract class AppDataBase {
+    abstract val barberoDao: BarberoDao
+    abstract val citaDao: CitaDao
+    abstract val perfilDao: PerfilDao
+    abstract val servicioDao: ServicioDao
+}
