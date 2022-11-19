094c861da8555fe579900a6861cbb8e3a8780288
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-11-19 11:28:27 -0400

    Update BarberoApi.kt

--- AppliedBarberShop/app/src/main/java/edu/ucne/appliedbarbershop/data/remote/api_dao/BarberoApi.kt
+++ AppliedBarberShop/app/src/main/java/edu/ucne/appliedbarbershop/data/remote/api_dao/BarberoApi.kt
@@ -7,18 +7,18 @@ import retrofit2.http.*
 interface BarberoApi {
 
     @GET("/api/Barberos")
-    suspend fun getAll(): Response<List<BarberoDto>>
+    suspend fun getAll(): List<BarberoDto>
 
     @GET("/api/Barberos/{id}")
-    suspend fun getById(@Path("id") id: String): Response<BarberoDto>
+    suspend fun getById(@Path("id") id: String): BarberoDto
 
     @POST("/api/Barberos")
-    suspend fun insert(@Body barbero: BarberoDto): Response<BarberoDto>
+    suspend fun insert(@Body barbero: BarberoDto): BarberoDto
 
     @DELETE("/api/Barberos/{id}")
-    suspend fun delete(@Path("id") id: String): Response<BarberoDto>
+    suspend fun delete(@Path("id") id: String): BarberoDto
 
     @PUT("/api/Barberos/{id}")
-    suspend fun update(@Path("id") id: String, @Body barbero: BarberoDto): Response<BarberoDto>
+    suspend fun update(@Path("id") id: String, @Body barbero: BarberoDto): BarberoDto
 
 }
