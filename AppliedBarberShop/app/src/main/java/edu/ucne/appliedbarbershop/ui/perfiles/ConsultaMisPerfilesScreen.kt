36e5560b0a8ab65cd09cf66d8db0775a9bd33f22
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-11-29 23:57:57 -0400

    Update ConsultaMisPerfilesScreen.kt

--- AppliedBarberShop/app/src/main/java/edu/ucne/appliedbarbershop/ui/perfiles/ConsultaMisPerfilesScreen.kt
+++ AppliedBarberShop/app/src/main/java/edu/ucne/appliedbarbershop/ui/perfiles/ConsultaMisPerfilesScreen.kt
@@ -13,12 +13,14 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import edu.ucne.appliedbarbershop.data.remote.dto.ClienteDto
 import edu.ucne.appliedbarbershop.ui.clientes.ClienteViewModel
 import edu.ucne.appliedbarbershop.ui.navegacion.NavegacionViewModel
 import edu.ucne.appliedbarbershop.ui.navegacion.TopBar
 import edu.ucne.appliedbarbershop.utils.Components.CardComponent
 import edu.ucne.appliedbarbershop.utils.Screen
 import kotlinx.coroutines.CoroutineScope
+import java.time.LocalDateTime
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,6 +32,7 @@ fun ConsultaMisClientesScreen(
     scope: CoroutineScope
 ) {
     val localContext = LocalContext.current
+
     Scaffold(
         topBar = {
             TopBar(
@@ -57,13 +60,25 @@ fun ConsultaMisClientesScreen(
             LazyColumn(modifier = Modifier.fillMaxWidth()) {
                 items(navegacionViewModel.perfiles) {
                     CardComponent(
-                        titulo = (it.nombre + " " + it.apellido) ?: "",
+                        titulo = (FormatearNombres(it.nombre, it.apellido)) ?: "",
                         cuerpo = "Celular: " + it.celular +
-                                "\nFecha de nacimiento: " + it.fechaNacimiento,
+                                "\nFecha de nacimiento: " + FormatearFecha(it.fechaNacimiento.toString()),
                         btn1Nombre = "Editar",
                         btn2Nombre = "Eliminar",
                         btn1OnClick = { navController.navigate(Screen.RegistroPerfilScreen.Route + "/" + it.perfilId.toString()) },
-                        btn2OnClick = {},
+                        btn2OnClick = { viewModel.delete( it.clienteId.toString(),
+                            cliente = ClienteDto(
+                                it.clienteId,
+                                it.nombre,
+                                it.apellido,
+                                it.celular,
+                                it.fechaNacimiento,
+                                it.imagen,
+                                LocalDateTime.now().toString(), // no se debe modificar
+                                LocalDateTime.now().toString(),
+                                0
+                            )
+                        )},
                         clickeable = true,
                         selected = (it.perfilId == navegacionViewModel.cliente.clienteId),
                         onClick = {
@@ -108,4 +123,19 @@ fun ConsultaMisClientesScreen(
             }
         }
     }
+}
+
+fun FormatearNombres(name: String, last : String) : String{
+     var nombreCompleto = name.replace("\\s".toRegex(), "") + " " + last.replace("\\s".toRegex(), "")
+    return  nombreCompleto
+}
+
+fun FormatearFecha(fecha: String) : String{
+    val delim = "T"
+    val palabras = fecha.split(delim)
+    var aux = ""
+
+    aux = palabras[0]
+
+    return aux
 }
